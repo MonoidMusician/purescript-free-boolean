bd42ef2a9115c198068459930440953d14a3f7fd
Nicholas Scheel <monoidmusician@gmail.com>
2018-02-27 23:49:26 -0600

    Cleanup

--- src/Data/BooleanAlgebra/CSS.purs
+++ src/Data/BooleanAlgebra/CSS.purs
@@ -5,24 +5,21 @@ import Prelude
 import Control.Alt ((<|>))
 import Control.Alternative (class Alternative, empty)
 import Control.Apply (lift5)
+import Control.MonadPlus (class MonadPlus)
 import Control.MonadZero (guard)
-import Data.Array (drop, mapMaybe)
-import Data.Bifoldable (class Bifoldable, bifoldMap, bifoldl, bifoldlDefault, bifoldr, bifoldrDefault)
-import Data.Bifunctor (class Bifunctor, bimap, rmap)
-import Data.Bitraversable (class Bitraversable, bisequenceDefault, bitraverse)
+import Data.Array (mapMaybe)
 import Data.BooleanAlgebra.NormalForm (NormalForm, toArrays, free)
-import Data.Foldable (class Foldable, all, foldM, foldMap, foldl, oneOfMap)
+import Data.Foldable (all, foldM, foldMap, foldl, oneOfMap)
 import Data.FoldableWithIndex (foldMapWithIndex)
 import Data.HeytingAlgebra (ff, tt)
+import Data.InterTsil (InterTsil(..), app)
 import Data.Lens (Iso', iso)
 import Data.Map (Map, singleton, unionWith)
 import Data.Maybe (Maybe(..))
 import Data.Monoid (mempty)
 import Data.Newtype (class Newtype, un, unwrap)
-import Data.Semigroup.Foldable (class Foldable1, fold1Default, foldMap1)
-import Data.Semigroup.Traversable (class Traversable1, sequence1Default, traverse1)
 import Data.String (joinWith)
-import Data.Traversable (class Traversable, sequence)
+import Data.Traversable (sequence)
 import Data.Tuple (Tuple(..))
 
 -- | A single portion of a selector. Covers atoms such as matching an element
@@ -181,48 +178,8 @@ type ASelector =
 newtype Atom = Atom ASelector
 derive instance newtypeAtom :: Newtype Atom _
 
-data InterTsil a b = One b | More (InterTsil a b) a b
-
-instance functorInterTsil :: Functor (InterTsil a) where
-  map = rmap
-instance bifunctorInterTsil :: Bifunctor InterTsil where
-  bimap _ g (One b) = One (g b)
-  bimap f g (More m a b) = More (bimap f g m) (f a) (g b)
-
-instance foldable1InterTsil :: Foldable1 (InterTsil a) where
-  foldMap1 g (One b) = g b
-  foldMap1 g (More m _ b) = foldMap1 g m <> g b
-  fold1 x = fold1Default x
-instance foldableInterTsil :: Foldable (InterTsil a) where
-  foldMap = bifoldMap mempty
-  foldl = bifoldl pure
-  foldr = bifoldr (flip pure)
-instance bifoldableInterTsil :: Bifoldable InterTsil where
-  bifoldMap _ g (One b) = g b
-  bifoldMap f g (More m a b) = bifoldMap f g m <> f a <> g b
-  bifoldl x = bifoldlDefault x
-  bifoldr x = bifoldrDefault x
-
-instance traversable1InterTsil :: Traversable1 (InterTsil a) where
-  traverse1 g (One b) = One <$> g b
-  traverse1 g (More m a b) = More <$> traverse1 g m <@> a <*> g b
-  sequence1 x = sequence1Default x
-instance traversableInterTsil :: Traversable (InterTsil a) where
-  traverse = bitraverse pure
-  sequence = bitraverse pure id
-instance bitraversableInterTsil :: Bitraversable InterTsil where
-  bitraverse _ g (One b) = One <$> g b
-  bitraverse f g (More m a b) = More <$> bitraverse f g m <*> f a <*> g b
-  bisequence x = bisequenceDefault x
-
-type Selectree = InterTsil Relation ASelector
-
-last :: forall a b. InterTsil a b -> b
-last (One s) = s
-last (More _ _ s) = s
-
 class Combinatorial c where
-  combine :: c -> c -> Array c
+  combine :: forall f. MonadPlus f => c -> c -> f c
   neutral :: c
 
 instance combinatorialAtom :: Combinatorial Atom where
@@ -234,35 +191,32 @@ derive instance newtypeRelated :: Newtype (Related a) _
 
 instance combinatorialRelated :: Combinatorial a => Combinatorial (Related a) where
   neutral = Related (One neutral)
-  combine (Related a) (Related b) = Related <$>
-    let
-      inner = case _, _ of
-        One s1, One s2 ->
-          One <$> combine s1 s2
-        One s1, More more rel s2 ->
-          More more rel <$> combine s1 s2
-        More more rel s1, One s2 ->
-          More more rel <$> combine s1 s2
-        More m1 r1 s1, More m2 r2 s2 ->
-          let
-            options =
-              inner m1 m2
-              <|> trySlide m1 r1 m2
-              <|> trySlide m2 r2 m1
-          in More <$> options <@> (r1 && r2) <*> combine s1 s2
+  combine (Related a) (Related b) = Related <$> inner a b where
+    inner = case _, _ of
+      One s1, One s2 ->
+        One <$> combine s1 s2
+      One s1, More more rel s2 ->
+        More more rel <$> combine s1 s2
+      More more rel s1, One s2 ->
+        More more rel <$> combine s1 s2
+      More m1 r1 s1, More m2 r2 s2 ->
+        let
+          options =
+            inner m1 m2
+            <|> trySlide m1 r1 m2
+            <|> trySlide m2 r2 m1
+        in More <$> options <@> (r1 && r2) <*> combine s1 s2
 
-      trySlide m1 true m2 =
-        drop 1 $ slide m1 m2
-      trySlide _ false _ = empty
+    trySlide m1 true m2 = slide m1 m2
+    trySlide _ false _ = empty
 
-      slide m1 (One s2) =
-        inner m1 (One s2) <|> pure (More m1 true s2)
-      slide m1 (More m2 r2 s2) =
-        inner m1 (More m2 r2 s2) <|> rest where
+    slide m1 = go where
+      go (One s2) = pure (More m1 true s2)
+      go (More m2 r2 s2) =
+        let
           btwn = guard r2 *> inner m1 (More m2 true neutral)
-          step = slide m1 m2
-          rest = More <$> (btwn <|> step) <@> r2 <@> s2
-    in inner a b
+          rest = inner m1 m2 <|> go m2
+        in More <$> (btwn <|> rest) <@> r2 <@> s2
 
 type Horiz = Related Atom
 type Vert = Related Horiz
@@ -284,55 +238,8 @@ imm (Related m) s = Related (More m false s)
 dist :: forall a. Related a -> a -> Related a
 dist (Related m) s = Related (More m true s)
 
-app :: forall a b. InterTsil a b -> a -> InterTsil a b -> InterTsil a b
-app m r (One s) = More m r s
-app m r (More m1 r1 s) = More (app m r m1) r1 s
-
 appR :: forall a. Related a -> Boolean -> Related a -> Related a
 appR (Related m1) r (Related m2) = Related (app m1 r m2)
-
-_i = Atom (matchClass "i") :: Atom
-_j = Atom (matchClass "j") :: Atom
-_k = Atom (matchClass "k") :: Atom
-_l = Atom (matchClass "l") :: Atom
-ijk = dist (dist (final _i) _j) _k :: Horiz
-_a = Atom (matchClass "a") :: Atom
-_b = Atom (matchClass "b") :: Atom
-test = combine (ijk) (dist (final _a) _b) :: Array Horiz
-tested = map printHoriz test :: Array String
-test1 = map printHoriz (combine (dist ijk _l) (dist (final _a) _b)) :: Array String
-
-midTest :: Array Horiz
-midTest =
-  let
-    q = Atom <<< matchClass
-    ab = dist (final (q "a")) (q "b")
-    cd = dist (final (q "c")) (q "d")
-    abcd = appR ab false cd
-    wxyz = dist (dist (imm (final (q "w")) (q "x")) (q "y")) (q "z")
-  in combine abcd wxyz
-
--- .a ~ .b > .c .d ~ .e + .f > .g && .h > .i ~ .j ~ .k .l + .m .n
-bigTest :: Array Vert
-bigTest =
-  let
-    q = Atom <<< matchClass
-    ab = dist (final (q "a")) (q "b")
-    c = final (q "c")
-    def = imm (dist (final (q "d")) (q "e")) (q "f")
-    g = final (q "g")
-    h = final (q "h")
-    ijk = dist (dist (final (q "i")) (q "j")) (q "k")
-    lm = imm (final (q "l")) (q "m")
-    n = final (q "n")
-    abcdefg = imm (dist (imm (final ab) c) def) g
-    hijklmn = dist (dist (imm (final h) ijk) lm) n
-  in combine abcdefg hijklmn
-
-bigTested :: Array String
-bigTested = map printVert bigTest
-
-bigTests = joinWith " || " bigTested :: String
 
 printHoriz :: Horiz -> String
 printHoriz (Related (One (Atom s))) = print1 s
@@ -399,96 +306,6 @@ selectToMatch' (Tuple v i) = case v of
   PseudoCls s -> matchPseudoCls' i s
   PseudoEl s -> matchPseudoEl' i s
   Attribute s -> matchAttr' i s
-
-
--- div.container .item && div.other .item
--- div.container.other .item || div.container div.other .item || div.other div.container .item
-
--- .a ~ .c && .b ~ .d
--- .a.b ~ .c.d || .a ~ .b ~ .c.d || .b ~ .a ~ .c.d
-
--- .a .c && .b > .c
--- .a.b > .c || .a .b > .c
-
--- .a (>) .c && .b (+~) .c -- sym
--- .a (>) .b (+~) .c
-
--- (div && .container || *:hover) > (h1 && .selected)
--- ((div && .container) > h1 || (*:hover) > h1) && .selected
--- ((div && .container) > h1) && .selected || ((*:hover) > h1) && .selected
--- ((div && .container) > (h1 && .selected)) || ((*:hover) > (h1 && .selected))
-
--- .a ~ .b > .c .d ~ .e + .f > .g && .h > .i ~ .j ~.k .l + .m .n
-
--- a ~ b + c + d ~ e + f && g ~ h + i ~ j + k
-
-{-
-combineRelations :: Selectree -> Selectree -> Array Selectree
-combineRelations (One s1) (One s2) = One <$> conjoin s1 s2
-combineRelations (One s1) (Related more rel s2) =
-  Related more rel <$> conjoin s1 s2
-combineRelations (Related more rel s1) (One s2) =
-  Related more rel <$> conjoin s1 s2
-combineRelations (Related m1 r1 s1) (Related m2 r2 s2) =
-  let
-    p1 = un Rel $ view rel r1
-    p2 = un Rel $ view rel r2
-  in case p1, p2 of
-    { vertical: true }, { vertical: false } ->
-      combineDisp m1 p1.immediate s1 m2 p2.immediate s2
-    { vertical: false }, { vertical: true } ->
-      combineDisp m2 p2.immediate s2 m1 p1.immediate s1
-    { immediate: true }, { immediate: true } ->
-      combineDirect p1.vertical m1 m2 s1 s2
-    { immediate: false }, { immediate: false } ->
-      combinePerm3 p1.vertical m1 m2 s1 s2
-    { immediate: false }, { immediate: true } ->
-      combinePerm2 p1.vertical m1 m2 s1 s2
-    { immediate: true }, { immediate: false } ->
-      combinePerm2 p1.vertical m2 m1 s2 s1
-
-mkV = if _ then Child else Descendant
-mkH = if _ then Next else Later
-mkI = if _ then Child else Next
-mkO = if _ then Descendant else Later
-
-combineDisp m1 i1 s1 m2 i2 s2 = pure $
-  Related (Related m1 (mkV i1) m2) (mkH i2) s2
-combineDirect v m1 m2 s1 s2 =
-  Related <$> combineRelations m1 m2 <@> mkI v <*> conjoin s1 s2
-combinePerm3 v m1 m2 s1 s2 =
-  let cnjnd = conjoin s1 s2
-  in  Related <$> combineRelations m1 m2 <@> mkO v <*> cnjnd
-  <|> Related (Related m1 (mkO v) m2) (mkO v) <$> cnjnd
-  <|> Related (Related m1 (mkO v) m2) (mkO v) <$> cnjnd
-combinePerm2 v m1 m2 s1 s2 =
-  let cnjnd = conjoin s1 s2
-  in  Related <$> combineRelations m1 m2 <@> mkI v <*> cnjnd
-  <|> Related (Related m1 (mkO v) m2) <$> cnjnd
-  <|> Related (Related m1 (mkO v) m2) <$> cnjnd
--}
-
-{-
-combineRelations (Rel a) (Rel b) =
-  let
-    combineDisp
-    combineDirect
-    combinePerm3
-    combinePerm2
-  in case a, b of
-    { vertical: true }, { vertical: false } ->
-      combineDisp a.immediate b.immediate
-    { vertical: false }, { vertical: true } ->
-      combineDisp b.immediate a.immediate
-    { immediate: true }, { immediate: true } ->
-      combineDirect a.vertical
-    { immediate: false }, { immediate: false } ->
-      combinePerm3 a.vertical
-    { immediate: false }, { immediate: true } ->
-      combinePerm2 a.vertical a b
-    { immediate: true }, { immediate: false } ->
-      combinePerm2 a.vertical b a
--}
 
 -- Prevent inconsistent values from appearing in the map
 dedup :: forall k v. Ord k => Eq v => Map k v -> Map k v -> Maybe (Map k v)
---
+++ src/Data/InterTsil.purs
@@ -0,0 +1,53 @@
+module Data.InterTsil where
+
+import Data.Bifunctor (class Bifunctor, bimap, rmap)
+import Data.Bitraversable (class Bifoldable, class Bitraversable, bifoldMap, bifoldl, bifoldlDefault, bifoldr, bifoldrDefault, bisequenceDefault, bitraverse)
+import Data.Monoid (mempty)
+import Data.Semigroup.Foldable (class Foldable1, fold1Default, foldMap1)
+import Data.Semigroup.Traversable (class Traversable1, sequence1Default, traverse1)
+import Data.Traversable (class Foldable, class Traversable)
+import Prelude (class Eq, class Functor, class Ord, flip, id, pure, (<$>), (<*>), (<>), (<@>))
+
+data InterTsil a b = One b | More (InterTsil a b) a b
+derive instance eqInterTsil :: (Eq a, Eq b) => Eq (InterTsil a b)
+derive instance ordInterTsil :: (Ord a, Ord b) => Ord (InterTsil a b)
+
+instance functorInterTsil :: Functor (InterTsil a) where
+  map = rmap
+instance bifunctorInterTsil :: Bifunctor InterTsil where
+  bimap _ g (One b) = One (g b)
+  bimap f g (More m a b) = More (bimap f g m) (f a) (g b)
+
+instance foldable1InterTsil :: Foldable1 (InterTsil a) where
+  foldMap1 g (One b) = g b
+  foldMap1 g (More m _ b) = foldMap1 g m <> g b
+  fold1 x = fold1Default x
+instance foldableInterTsil :: Foldable (InterTsil a) where
+  foldMap = bifoldMap mempty
+  foldl = bifoldl pure
+  foldr = bifoldr (flip pure)
+instance bifoldableInterTsil :: Bifoldable InterTsil where
+  bifoldMap _ g (One b) = g b
+  bifoldMap f g (More m a b) = bifoldMap f g m <> f a <> g b
+  bifoldl x = bifoldlDefault x
+  bifoldr x = bifoldrDefault x
+
+instance traversable1InterTsil :: Traversable1 (InterTsil a) where
+  traverse1 g (One b) = One <$> g b
+  traverse1 g (More m a b) = More <$> traverse1 g m <@> a <*> g b
+  sequence1 x = sequence1Default x
+instance traversableInterTsil :: Traversable (InterTsil a) where
+  traverse = bitraverse pure
+  sequence = bitraverse pure id
+instance bitraversableInterTsil :: Bitraversable InterTsil where
+  bitraverse _ g (One b) = One <$> g b
+  bitraverse f g (More m a b) = More <$> bitraverse f g m <*> f a <*> g b
+  bisequence x = bisequenceDefault x
+
+last :: forall a b. InterTsil a b -> b
+last (One s) = s
+last (More _ _ s) = s
+
+app :: forall a b. InterTsil a b -> a -> InterTsil a b -> InterTsil a b
+app m r (One s) = More m r s
+app m r (More m1 r1 s) = More (app m r m1) r1 s
