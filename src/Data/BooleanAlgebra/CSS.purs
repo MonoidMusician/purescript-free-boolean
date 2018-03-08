34558b1a76b734042bf4c0bed11ca3b05643fd2d
Nicholas Scheel <monoidmusician@gmail.com>
2018-03-08 15:19:43 -0600

    Refactor combinatorial into smaller parts

--- src/Data/BooleanAlgebra/CSS.purs
+++ src/Data/BooleanAlgebra/CSS.purs
@@ -3,13 +3,10 @@ module Data.BooleanAlgebra.CSS where
 import Prelude
 
 import Control.Alt ((<|>))
-import Control.Alternative (class Alternative, empty)
 import Control.Apply (lift5)
-import Control.MonadPlus (class MonadPlus)
-import Control.MonadZero (guard)
-import Data.Array (mapMaybe)
+import Control.MonadPlus (class MonadPlus, empty, guard)
 import Data.BooleanAlgebra.NormalForm (NormalForm, toArrays, free)
-import Data.Foldable (all, foldM, foldMap, foldl, oneOfMap)
+import Data.Foldable (all, foldMap, foldl, oneOfMap)
 import Data.FoldableWithIndex (foldMapWithIndex)
 import Data.HeytingAlgebra (ff, tt)
 import Data.InterTsil (InterTsil(..), concat)
@@ -19,7 +16,7 @@ import Data.Maybe (Maybe(..))
 import Data.Monoid (mempty)
 import Data.Newtype (class Newtype, un, unwrap)
 import Data.String (joinWith)
-import Data.Traversable (sequence)
+import Data.Traversable (class Foldable, sequence)
 import Data.Tuple (Tuple(..))
 
 -- | A single portion of a selector. Covers atoms such as matching an element
@@ -95,6 +92,7 @@ derive instance ordMatchValueType :: Ord MatchValueType
 
 -- | A full CSS selector represented as a free boolean algebra over the
 -- | selector atoms.
+-- S.Map (InterList Relation (M.Map Select Boolean)) Boolean
 newtype Selector = S (NormalForm Select)
 derive instance newtypeSelector :: Newtype Selector _
 derive newtype instance heytingAlgebrCSSelector :: HeytingAlgebra Selector
@@ -161,33 +159,81 @@ exactAttr a v = attrValue a $ MatchValue
 attrValue :: String -> MatchValue -> Selector
 attrValue a v = S $ free $ Attribute $ AttrMatch { attr: a, match: Just v }
 
+newtype Single a = Single (Maybe { inverted :: Boolean, value :: a })
+derive instance newtypeSingle :: Newtype (Single a) _
+instance eqSingle :: Eq a => Eq (Single a) where
+  eq (Single (Just a)) (Single (Just b)) =
+    a.inverted == b.inverted && a.value == b.value
+  eq (Single Nothing) (Single Nothing) = true
+  eq _ _ = false
+
+newtype Several a = Several (Map a Boolean)
+derive instance newtypeSeveral :: Newtype (Several a) _
+derive newtype instance eqSeveral :: Ord a => Eq (Several a)
+
 -- A single (conjunctive) selector
 type ASelector =
-  { element :: Maybe
-    { inverted :: Boolean
-    , name :: String
-    }
-  , classes :: Map String Boolean
-  , pseudoCls :: Map String Boolean
-  , pseudoEl :: Maybe
-    { inverted :: Boolean
-    , name :: String
-    }
-  , attrs :: Map AttrMatch Boolean
+  { element   :: Single  String
+  , classes   :: Several String
+  , pseudoCls :: Several String
+  , attrs     :: Several AttrMatch
+  , pseudoEl  :: Single String
   }
 newtype Atom = Atom ASelector
 derive instance newtypeAtom :: Newtype Atom _
+derive instance eqAtom :: Eq Atom
 
-class Combinatorial c where
+class Eq c <= Combinatorial c where
   combine :: forall f. MonadPlus f => c -> c -> f c
   neutral :: c
 
+-- Conjoin many conjunctive clauses together, doing case analysis as necesssary
+-- or dropping impossible conjunctions.
+combineFold ::
+  forall f g c.
+    Combinatorial c =>
+    Foldable f => MonadPlus g =>
+  f c -> g c
+combineFold = foldl (\fb a -> fb >>= \b -> combine b a) (pure neutral)
+
+instance combinatorialUnit :: Combinatorial Unit where
+  combine _ _ = pure neutral
+  neutral = unit
+
 instance combinatorialAtom :: Combinatorial Atom where
-  combine (Atom a1) (Atom a2) = Atom <$> conjoin a1 a2
-  neutral = Atom matchAll
+  combine (Atom a) (Atom b) = Atom <$> lift5
+    { element: _, attrs: _, classes: _, pseudoCls: _, pseudoEl: _ }
+    (combine a.element b.element)
+    (combine a.attrs b.attrs)
+    (combine a.classes b.classes)
+    (combine a.pseudoCls b.pseudoCls)
+    (combine a.pseudoEl b.pseudoEl)
+  neutral = matchAll
+
+instance combinatorialSingle :: Eq a => Combinatorial (Single a) where
+  neutral = Single Nothing
+  combine (Single Nothing) v = pure v
+  combine v (Single Nothing) = pure v
+  combine l@(Single (Just a)) r@(Single (Just b)) =
+    case a.inverted, b.inverted of
+      false, true -> pure l
+      true, false -> pure r
+      _, _ | a.value == b.value -> pure l
+           | otherwise -> empty
+
+instance combinatorialSeveral :: Ord a => Combinatorial (Several a) where
+  neutral = Several mempty
+  combine (Several a) (Several b) =
+    oneOfMap (pure <<< Several) $ sequence $
+      unionWith agreement (a <#> Just) (b <#> Just)
+    where
+      agreement (Just true) (Just true) = Just true
+      agreement (Just false) (Just false) = Just false
+      agreement _ _ = Nothing
 
 newtype Related a = Related (InterTsil Boolean a)
 derive instance newtypeRelated :: Newtype (Related a) _
+derive newtype instance eqRelated :: Eq a => Eq (Related a)
 
 instance combinatorialRelated :: Combinatorial a => Combinatorial (Related a) where
   neutral = Related (One neutral)
@@ -221,16 +267,6 @@ instance combinatorialRelated :: Combinatorial a => Combinatorial (Related a) wh
 type Horiz = Related Atom
 type Vert = Related Horiz
 
--- i ~ j ~ k && a ~ b
--- ==
--- a ~ i ~ j ~ kb
--- ai ~ j ~ kb
--- i ~ a ~ j ~ kb : combineH (More (One _i) true matchAll) (One _a) <#> \m ->
-
--- i ~ aj ~ kb
-
--- i ~ j ~ a ~ kb
-
 final :: forall a. a -> Related a
 final = Related <<< One
 imm :: forall a. Related a -> a -> Related a
@@ -242,8 +278,8 @@ appR :: forall a. Related a -> Boolean -> Related a -> Related a
 appR (Related m1) r (Related m2) = Related (concat m1 r m2)
 
 printHoriz :: Horiz -> String
-printHoriz (Related (One (Atom s))) = print1 s
-printHoriz (Related (More m r (Atom s))) = printHoriz (Related m) <> (if r then " ~ " else " + ") <> print1 s
+printHoriz (Related (One s)) = print1 s
+printHoriz (Related (More m r s)) = printHoriz (Related m) <> (if r then " ~ " else " + ") <> print1 s
 
 printVert :: Vert -> String
 printVert (Related (One h)) = printHoriz h
@@ -252,54 +288,55 @@ printVert (Related (More m r h)) =
   <> (if r then " " else " > ")
   <> printHoriz h
 
--- A disjunction of many selectors
-type SomeSelectors = Array ASelector
+-- A disjunction of Several selectors
+type SomeSelectors = Array Atom
 
 -- Identity selector
-matchAll :: ASelector
-matchAll =
-  { element: Nothing
-  , classes: mempty
-  , pseudoCls: mempty
-  , pseudoEl: Nothing
-  , attrs: mempty
+matchAll' :: ASelector
+matchAll' =
+  { element: neutral
+  , classes: neutral
+  , pseudoCls: neutral
+  , pseudoEl: neutral
+  , attrs: neutral
   }
+matchAll = Atom matchAll' :: Atom
 
-matchElement :: String -> ASelector
+matchElement :: String -> Atom
 matchElement = matchElement' false
-matchClass :: String -> ASelector
+matchClass :: String -> Atom
 matchClass = matchClass' false
-matchPseudoCls :: String -> ASelector
+matchPseudoCls :: String -> Atom
 matchPseudoCls = matchPseudoCls' false
-matchPseudoEl :: String -> ASelector
+matchPseudoEl :: String -> Atom
 matchPseudoEl = matchPseudoEl' false
-matchAttr :: AttrMatch -> ASelector
+matchAttr :: AttrMatch -> Atom
 matchAttr = matchAttr' false
 
-matchElement' :: Boolean -> String -> ASelector
-matchElement' inverted name = matchAll
-  { element = Just { inverted, name } }
+matchElement' :: Boolean -> String -> Atom
+matchElement' inverted name = Atom $ matchAll'
+  { element = Single $ Just { inverted, value: name } }
 
-matchClass' :: Boolean -> String -> ASelector
-matchClass' inverted name = matchAll
-  { classes = singleton name inverted }
+matchClass' :: Boolean -> String -> Atom
+matchClass' inverted name = Atom $ matchAll'
+  { classes = Several $ singleton name inverted }
 
-matchPseudoCls' :: Boolean -> String -> ASelector
-matchPseudoCls' inverted name = matchAll
-  { pseudoCls = singleton name inverted }
+matchPseudoCls' :: Boolean -> String -> Atom
+matchPseudoCls' inverted name = Atom $ matchAll'
+  { pseudoCls = Several $ singleton name inverted }
 
-matchPseudoEl' :: Boolean -> String -> ASelector
-matchPseudoEl' inverted name = matchAll
-  { pseudoEl = Just { inverted, name } }
+matchPseudoEl' :: Boolean -> String -> Atom
+matchPseudoEl' inverted name = Atom $ matchAll'
+  { pseudoEl = Single $ Just { inverted, value: name } }
 
-matchAttr' :: Boolean -> AttrMatch -> ASelector
-matchAttr' inverted match = matchAll
-  { attrs = singleton match inverted }
+matchAttr' :: Boolean -> AttrMatch -> Atom
+matchAttr' inverted match = Atom $ matchAll'
+  { attrs = Several $ singleton match inverted }
 
-selectToMatch :: Select -> ASelector
+selectToMatch :: Select -> Atom
 selectToMatch = selectToMatch' <<< (Tuple <@> false)
 
-selectToMatch' :: Tuple Select Boolean -> ASelector
+selectToMatch' :: Tuple Select Boolean -> Atom
 selectToMatch' (Tuple v i) = case v of
   Element s -> matchElement' i s
   Class s -> matchClass' i s
@@ -307,42 +344,8 @@ selectToMatch' (Tuple v i) = case v of
   PseudoEl s -> matchPseudoEl' i s
   Attribute s -> matchAttr' i s
 
--- Prevent inconsistent values from appearing in the map
-dedup :: forall k v. Ord k => Eq v => Map k v -> Map k v -> Maybe (Map k v)
-dedup l r = sequence $ unionWith merger (l <#> Just) (r <#> Just)
-  where
-    merger :: Maybe v -> Maybe v -> Maybe v
-    merger (Just a) (Just b) | a == b = Just a
-    merger _ _ = Nothing
-
--- Conjoin two selectors, returning `Nothing` if the result is provably `false`
--- (e.g. if it would have to match two different element types or pseudo-element
--- types at once).
-conjoin :: forall f. Alternative f => ASelector -> ASelector -> f ASelector
-conjoin a b = oneOfMap pure $ lift5
-  { element: _, attrs: _, classes: _, pseudoCls: _, pseudoEl: _ }
-  (combE a.element b.element)
-  (dedup a.attrs b.attrs)
-  (dedup a.classes b.classes)
-  (dedup a.pseudoCls b.pseudoCls)
-  (combE a.pseudoEl b.pseudoEl)
-  where
-    combE Nothing v = Just v
-    combE v Nothing = Just v
-    combE l@(Just a) r@(Just b) =
-      case a.inverted, b.inverted of
-        false, true -> Just l
-        true, false -> Just r
-        _, _ | a.name == b.name -> Just l
-             | otherwise -> Nothing
-
--- Conjoin many selectors
-conjoinFold :: SomeSelectors -> Maybe ASelector
-conjoinFold = foldl (flip (foldM conjoin)) (Just matchAll)
-
 fromNF :: NormalForm Select -> SomeSelectors
-fromNF = unwrap >>> toArrays >>> mapMaybe
-  (map selectToMatch' >>> conjoinFold)
+fromNF = unwrap >>> toArrays >=> map selectToMatch' >>> combineFold
 
 ensure :: String -> String -> String
 ensure s "" = s
@@ -352,17 +355,17 @@ ensure _ s = s
 print :: SomeSelectors -> String
 print = ensure ":not(*)" <<< joinWith ", " <<< map print1
 
-print1 :: ASelector -> String
-print1 v = ensure "*" $ e <> as <> c <> pc <> pe where
+print1 :: Atom -> String
+print1 (Atom v) = ensure "*" $ e <> as <> c <> pc <> pe where
   invert false n = n
   invert true n = ":not(" <> n <> ")"
   -- Print the element, or *
   e = case v.element of
-    Nothing -> ""
-    Just { inverted, name } ->
-      invert inverted name
+    Single Nothing -> ""
+    Single (Just { inverted, value }) ->
+      invert inverted value
   -- Print attributes in brackets[]
-  as = v.attrs # foldMapWithIndex \(AttrMatch { attr, match }) i ->
+  as = v.attrs # unwrap # foldMapWithIndex \(AttrMatch { attr, match }) i ->
     invert i case match of
       Nothing -> "[" <> attr <> "]"
       Just (MatchValue { matchType, value, insensitive }) ->
@@ -377,11 +380,11 @@ print1 v = ensure "*" $ e <> as <> c <> pc <> pe where
             LangCode -> "|"
         in "[" <> attr <> op <> "=" <> show value <> i' <> "]"
   -- Print classes, preceded by periods.
-  c = v.classes # foldMapWithIndex \name i ->
+  c = v.classes # unwrap # foldMapWithIndex \name i ->
     invert i ("." <> name)
   -- Print pseudo-classes, preceded by colons:
-  pc = v.pseudoCls # foldMapWithIndex \name i ->
+  pc = v.pseudoCls # unwrap # foldMapWithIndex \name i ->
     invert i (":" <> name)
   -- Print the pseudo-element (if applicable), preceded by a double colon::
-  pe = v.pseudoEl # foldMap \{ inverted, name } ->
-    invert inverted ("::" <> name)
+  pe = v.pseudoEl # unwrap # foldMap \{ inverted, value } ->
+    invert inverted ("::" <> value)
